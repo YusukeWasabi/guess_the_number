a7d7108387d80485b251388a9110aba3168e61dd
YusukeWasabi <dan7kidz@gmail.com>
2020-10-31 15:26:47 +0700

    Create Number Guessing Game

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,17 +1,17 @@
-player1 = str(input("Enter Your Name:"))#takes input of player's name
-print('Welcome', '' ,player1)
+player1 = str(input("Enter Your Name: "))#takes input of player's name
+print('\nWelcome', '' ,player1)
 import random  #importing random from library
 
 number = random.randint(1,6) #setting range of random module
 
 attempts = 0 #count of attempts made by player to guess the number
 guess = 0  #takes input of the guessed number by player
-        
-             
+
+
 while guess != number:
          try:
-            guess = eval(str(input('Guess number between 1 to 6:'))) #evaluates the guess and number from random module
-         except NameError: 
+            guess = eval(str(input('\nGuess number between 1 to 6:'))) #evaluates the guess and number from random module
+         except NameError:
                  print('enter only integers') #checks out strings input
                  continue
          except SyntaxError:
@@ -23,13 +23,12 @@ while guess != number:
 
 
          attempts += 1 #count of attempts by player
-         
+
          if guess == number:
                  print ('Correct! You used', attempts, 'attempts!')
                  break
-         
+
          if guess < number:
                   print ('Go higher!')
          if guess > number:
                    print ('Go lower!')
-                   
